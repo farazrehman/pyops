5f5ba7a8a3b95870e0091bfa7d67c929f8142da3
farazrehman <farazr@10.85.18.238>
2016-04-30 14:55:01 -0400

    Initial checkin of the file

---
+++ enter-codes.py
@@ -0,0 +1,13 @@
+
+
+print "Welcome to AP Codes - Data Entry"
+
+
+code = "m"
+
+while code !="qq":
+	code = raw_input ("Enter IATA Code: ")
+	name = raw_input ("Enter Name:      ")
+	print ("You Entered "+code+" as "+name)
+	
+	
